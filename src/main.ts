be4fb539d96c6d0be9e24cd70c012073e5f3ced8
coder-itl <ituninapp@gmail.com>
2024-07-15 22:35:50 +0800

    feat: 添加全屏插件

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 // 如果只想导入css变量
@@ -17,6 +18,9 @@ const app = createApp(App)
 app.use(ElementPlus, {
     locale: zhCn,
 });
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(createPinia())
 app.use(router)
 
